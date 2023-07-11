73d0688b385f0e4031927b0c31020cee0317cf5c
pablo014 <angelopablo014@gmail.com>
2023-07-11 16:48:19 -0700

    feat(comp): add preview card

---
+++ src/PreviewCard.vue
@@ -0,0 +1,56 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "PreviewCard",
+    props: {
+        image: {
+            type: String,
+            default: ""
+        },
+        title: {
+            type: String,
+            default: "Preview"
+        },
+        summary: {
+            type: String,
+            default: "Preview"
+        },
+        url: {
+            type: String,
+            required: false
+        }
+    }
+})
+</script>
+
+<template>
+    <a id="container" :href="url">
+        <img :src="image" :alt="title" id="image"/>
+        <div id="title">
+            {{ title }}
+        </div>
+        <div id="summary">
+            {{ summary }}
+        </div>
+    </a>
+</template>
+
+<style scoped>
+#container {
+    border-radius: 10px;
+    border-color: #9E9E9E;
+    border-width: 3px;
+    cursor: pointer;
+    transition-property: all;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 500ms;
+}
+#container:hover {
+    scale: 110%;
+    box-shadow: 0 10px 5px darkgray;
+}
+#image {
+    border-radius: 10px;
+}
+</style>
--- src/components.js
+++ src/components.js
@@ -1,4 +1,5 @@
 import InputText from './InputText.vue'
 import InputTextarea from './InputTextarea.vue'
+import PreviewCard from "./PreviewCard.vue";
 
-export default { InputTextarea, InputText }
+export default { InputTextarea, InputText, PreviewCard }
