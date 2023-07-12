9b389b769d1a2d7830496b026495c4e04401d7b8
pablo014 <angelopablo014@gmail.com>
2023-07-11 19:39:51 -0700

    feat(inputText): add stylings for input text

--- src/InputText.vue
+++ src/InputText.vue
@@ -1,15 +1,39 @@
-<script>
-import {defineComponent} from 'vue'
-
-export default defineComponent({
-    name: "InputText"
-})
-</script>
-
 <template>
-
+    <div class="input-container">
+        <input
+                type="text"
+                v-model="inputValue"
+                class="input-text"
+                placeholder="Enter text..."
+        />
+    </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+    data() {
+        return {
+            inputValue: ''
+        };
+    }
+};
+</script>
 
+<style>
+.input-container {
+    width: 300px;
+}
+
+.input-text {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+}
+
+.input-text:focus {
+    outline: none;
+    border-color: dodgerblue;
+}
 </style>
