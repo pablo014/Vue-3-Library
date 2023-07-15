311e35842580c53561f7b578a7725d992469057b
pablo014 <angelopablo014@gmail.com>
2023-07-14 23:09:45 -0700

    feat(comps): add new comps to export

--- src/components.js
+++ src/components.js
@@ -1,5 +1,8 @@
 import InputText from './InputText.vue'
 import InputTextarea from './InputTextarea.vue'
 import PreviewCard from "./PreviewCard.vue";
+import Pagination from "./Pagination.vue";
+import Table from "./Table.vue";
+import Button from "./Button.vue";
 
-export default { InputTextarea, InputText, PreviewCard }
+export default { InputTextarea, InputText, PreviewCard, Pagination, Table, Button }
