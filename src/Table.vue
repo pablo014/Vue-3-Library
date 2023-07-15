f10e008521dc2e8974a6afc24f2db40335bfae30
pablo014 <angelopablo014@gmail.com>
2023-07-14 19:25:15 -0700

    feat(comps): add table

---
+++ src/Table.vue
@@ -0,0 +1,55 @@
+<template>
+    <table class="table">
+        <thead>
+        <tr>
+            <th v-for="column in columns" :key="column.key">{{ column.label }}</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in items" :key="item.id">
+            <td v-for="column in columns" :key="column.key">{{ item[column.key] }}</td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    props: {
+        columns: {
+            type: Array,
+            required: true
+        },
+        items: {
+            type: Array,
+            required: true
+        }
+    }
+};
+</script>
+
+<style>
+.table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+.table th,
+.table td {
+    padding: 10px;
+    border: 1px solid #ccc;
+}
+
+.table th {
+    font-weight: bold;
+    background-color: #f0f0f0;
+}
+
+.table tr:hover {
+    background-color: #f9f9f9;
+}
+
+.table td {
+    text-align: left;
+}
+</style>
