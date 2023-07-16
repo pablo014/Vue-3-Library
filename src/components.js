8a77b96a0d200aa723e415058d372bb74952cf66
pablo014 <angelopablo014@gmail.com>
2023-07-16 13:55:41 -0700

    fix(comps): add to comp file

--- src/components.js
+++ src/components.js
@@ -4,5 +4,8 @@ import PreviewCard from "./PreviewCard.vue";
 import Pagination from "./Pagination.vue";
 import Table from "./Table.vue";
 import Button from "./Button.vue";
+import Accordion from "./Accordion.vue";
+import Dropdown from "./Dropdown.vue";
 
-export default { InputTextarea, InputText, PreviewCard, Pagination, Table, Button }
+
+export default { InputTextarea, InputText, PreviewCard, Pagination, Table, Button, Accordion, Dropdown }
